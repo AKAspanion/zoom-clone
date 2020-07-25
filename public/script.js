404f20726d1c3a797fdb60873acd9ea3a61b23bc
Ankit Kumar <panditankit1995@gmail.com>
2020-07-26 03:54:04 +0530

    ✨ server setup

--- public/script.js
+++ public/script.js
@@ -1,7 +1,66 @@
 const socket = io("/");
-
-socket.emit("join-room", ROOM_ID, 10);
-
-socket.on("user-connected", (id) => {
-  console.log(id);
+const peer = new Peer(undefined, {
+  secure: true,
+  host: "spanion-video-chat-peer.herokuapp.com",
 });
+
+const peers = {};
+const videoGrid = document.getElementById("video-grid");
+
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+const video = document.createElement("video");
+video.muted = true;
+
+const mediaConfig = {
+  video: true,
+  audio: true,
+};
+navigator.mediaDevices
+  .getUserMedia(mediaConfig)
+  .then((stream) => {
+    addVideoStream(video, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+
+      const video = document.createElement("video");
+      call.on("stream", (userStream) => {
+        addVideoStream(video, userStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
+  })
+  .catch((err) => {
+    document.write(err);
+  });
+
+socket.on("user-disconnected", (userId) => {
+  if (peers[userId]) peers[userId].close();
+});
+
+function connectToNewUser(userId, stream) {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userStream) => {
+    addVideoStream(video, userStream);
+  });
+  call.on("close", () => {
+    video.remove();
+  });
+
+  peers[userId] = call;
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
--- server.js
+++ server.js
@@ -27,6 +27,10 @@ io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
     socket.to(roomId).broadcast.emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
   });
 });
 
